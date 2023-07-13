47dd7140922966dd27b0678611aa397f697f9c2e
nishu-murmu <nishumurmu017@gmail.com>
2023-07-13 10:08:06 +0530

    fix: showing initial loading

--- src/options/components/Layout.tsx
+++ src/options/components/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Form from './generic/Form'
 import Tabs from './Tabs'
 import { useLocalStorage } from '../hooks/use-localStorage'
@@ -7,11 +7,13 @@ import { useSupabase } from '../hooks/use-Supabase'
 
 export default function Layout() {
   const { getLocalStorage } = useLocalStorage()
+  const [loading, setLoading] = useState(false)
   const { signOut } = useSupabase()
   const navigate = useNavigate()
 
   useEffect(() => {
     const response = getLocalStorage('user')
+    setLoading(true)
     const authResponse = getLocalStorage('sb-fxwbkyonnbbvdnqbmppu-auth-token')
     if (!response?.email && !authResponse?.user?.id) {
       navigate('/login')
@@ -19,12 +21,16 @@ export default function Layout() {
   }, [])
 
   return (
-    <div className="flex bg-custom_white flex-col items-center min-h-screen">
-      <div className="mt-14 mb-8">
-        <Tabs />
-      </div>
-      <Form />
-      <button onClick={signOut}>logout</button>
-    </div>
+    <>
+      {loading && (
+        <div className="flex bg-custom_white flex-col items-center min-h-screen">
+          <div className="mt-14 mb-8">
+            <Tabs />
+          </div>
+          <Form />
+          <button onClick={signOut}>logout</button>
+        </div>
+      )}
+    </>
   )
 }
