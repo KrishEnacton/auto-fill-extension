dd9f2db3239ccb92d39bc908935803390a870a18
MadhuSaini22 <madhusaini46810@gmail.com>
2023-07-18 13:46:22 +0530

    fix: errors in education

--- src/options/components/core/ErrorText.tsx
+++ src/options/components/core/ErrorText.tsx
@@ -10,7 +10,6 @@ const ErrorText: React.FC<{ error?: string; touched?: boolean; education?: Educa
     <>
       {education ? (
         <>
-          ffer
           {error ? <div className="mt-2 ml-1 text-xs text-red-500 text-left">{error}</div> : null}
         </>
       ) : (
