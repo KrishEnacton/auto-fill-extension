a4f52e54584e664ef1af63eb56ae5cf9830ba4e9
nishu-murmu <nishumurmu017@gmail.com>
2023-08-16 19:48:17 +0530

    fix: icims autofilling

--- src/content/Popup/websites/iCIMS/index.ts
+++ src/content/Popup/websites/iCIMS/index.ts
@@ -19,8 +19,91 @@ function normalFieldsAutoFill(inputValue: string, InputElem: Element | null) {
   }
 }
 
-function selectFieldAutoFill() {}
+function ExprienceAutoFill(iframeParent: any, UserDetails: any) {
+  const experience: any = getInputValue('experience', UserDetails)
 
+  const experienceList = experience?.[1]
+
+  for (const i in experienceList) {
+    for (const [key, value] of Object.entries(iCIMSConfig.selectors)) {
+      iframeParent
+        .querySelector(`div[class*="rcf3211-${Number(i) + 1}-"]`)
+        .querySelectorAll('input,textarea')
+        .forEach((item: any) => {
+          if (item.type == 'hidden') return
+
+          console.log(item)
+          const text =
+            item.parentElement.firstChild.innerText !== ''
+              ? item.parentElement.firstChild.innerText
+              : item.parentElement.previousElementSibling.innerText
+
+          if (text != null) {
+            console.log({ text })
+            const result: any = Object.entries(iCIMSConfig.selectors).find((item) => {
+              if (new RegExp(`\\b${item[1]}`).test(text)) return item
+            })
+            const inputValue: any = getInputValue(result?.[0], experienceList[i])
+
+            if (text != null && new RegExp(`\\b${value}`, 'i').test(text)) {
+              normalFieldsAutoFill(inputValue?.[1], item)
+            }
+          }
+        })
+      // iframeParent
+      //   .querySelector(`div[class*="rcf3211-${Number(i) + 1}-"]`)
+      //   .querySelectorAll('select')
+      //   .forEach((item: any) => {
+      //     const inputValue: any = getInputValue(key, UserDetails)
+      //     const text = item.previousElementSibling.innerText
+      //     if (text != null && new RegExp(`\\b${value}`, 'i').test(text)) {
+      //       console.log('check')
+      //     }
+      //   })
+    }
+  }
+}
+
+function educationAutoFill(iframeParent: any, UserDetails: any) {
+  for (const [key, value] of Object.entries(iCIMSConfig.selectors)) {
+    iframeParent
+      .querySelector(iCIMSConfig.education)
+      .querySelectorAll('select')
+      .forEach((item: any) => {
+        const inputValue: any = getInputValue(key, UserDetails)
+        const text = item.previousElementSibling.innerText
+        if (item !== null) {
+          const parentElem = item.parentElement
+          const prevLabelElem = parentElem.querySelector('label')
+          if (
+            prevLabelElem?.innerText != null &&
+            new RegExp(`\\b${value}`).test(prevLabelElem.innerText)
+          ) {
+            const dropdownElem = parentElem.querySelector(iCIMSConfig.dropdownSelector)
+            if (dropdownElem != null) {
+              dropdownElem.classList.remove('dropdown-invisible')
+              dropdownElem.ariaExpanded = 'true'
+              let value =
+                typeof inputValue?.[1] == 'string' ? inputValue?.[1] : inputValue?.[1].name
+              if (value) {
+                if (inputValue[0] == 'ethnicity') {
+                  value = value.includes('/') ? value.split('/') : value
+                  value = value.includes(' ') ? value.split(' ').join('|') : value
+                }
+                Array.from(parentElem.querySelectorAll('ul > li')).forEach((li: any) => {
+                  if (new RegExp(`\\b${value}`).test(li.innerText)) {
+                    li.click()
+                  }
+                })
+              }
+              dropdownElem.classList.add('dropdown-invisible')
+              dropdownElem.ariaExpanded = 'false'
+            }
+          }
+        }
+      })
+  }
+}
 // website specific auto-filling function
 export const iCIMSAutoFilling = (userInfo: UserInfo) => {
   const UserDetails = spreadUserInfo(userInfo)
@@ -59,7 +142,7 @@ export const iCIMSAutoFilling = (userInfo: UserInfo) => {
         }
       }
 
-      // for normal select
+      // for select dropdown
       for (const item of Array.from(iframeParent.querySelectorAll(iCIMSConfig.commonSelector))) {
         let Item: any = item
         const selectElem: any = Item.querySelector('select')
@@ -133,6 +216,9 @@ export const iCIMSAutoFilling = (userInfo: UserInfo) => {
       }
     }
 
-    console.log('check kar')
+    setTimeout(() => {
+      ExprienceAutoFill(iframeParent, UserDetails)
+      educationAutoFill(iframeParent, UserDetails)
+    })
   }, 3000)
 }
