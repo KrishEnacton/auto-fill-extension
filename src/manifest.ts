348e8dc19a84ac13ea89bb56f6e681c4fb371d62
nishu-murmu <nishumurmu017@gmail.com>
2023-09-25 14:42:13 +0530

    refact: major version update.

--- src/manifest.ts
+++ src/manifest.ts
@@ -3,7 +3,7 @@ import { defineManifest } from '@crxjs/vite-plugin'
 export default defineManifest({
   name: 'Auto Fill',
   description: '',
-  version: '0.3.0',
+  version: '0.4.0',
   manifest_version: 3,
   icons: {
     '16': 'img/logo-16.png',
